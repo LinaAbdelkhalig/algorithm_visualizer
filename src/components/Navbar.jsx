ec707d93320839648c59f4cd31f73c4942c1425b
Lina Abdelkhalig <lelsammani0@gmail.com>
2025-01-10 19:02:24 +0200

    added dropdown to nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Navbar({ onResetArray, onMergeSort, onQuickSort }) {
+    // State to track the selected algorithm
+    const [selectedAlgorithm, setSelectedAlgorithm] = useState("Select Algorithm");
+
+    // Handle sorting based on the selected algorithm
+    const handleSort = () => {
+        if (selectedAlgorithm === "Merge Sort") {
+            onMergeSort();
+        } else if (selectedAlgorithm === "Quick Sort") {
+            onQuickSort();
+        } else {
+            alert("Please select a sorting algorithm!");
+        }
+    };
+
     return (
-        <nav class="navbar navbar-expand-lg">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Sorting Visualizer</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
                             <button className="btn btn-outline-primary mx-2" onClick={onResetArray}>Generate a New Array</button>
                         </li>
-                        <li class="nav-item">
-                            <button className="btn btn-outline-primary mx-2" onClick={onMergeSort}>Merge Sort</button>
+                        <li className="nav-item dropdown">
+                            <button
+                                className="btn btn-outline-primary dropdown-toggle mx-2"
+                                id="algorithmDropdown"
+                                data-bs-toggle="dropdown"
+                                aria-expanded="false"
+                            >
+                                {selectedAlgorithm}
+                            </button>
+                            <ul className="dropdown-menu" aria-labelledby="algorithmDropdown">
+                                <li>
+                                    <button
+                                        className="dropdown-item"
+                                        onClick={() => setSelectedAlgorithm("Merge Sort")}
+                                    >
+                                        Merge Sort
+                                    </button>
+                                </li>
+                                <li>
+                                    <button
+                                        className="dropdown-item"
+                                        onClick={() => setSelectedAlgorithm("Quick Sort")}
+                                    >
+                                        Quick Sort
+                                    </button>
+                                </li>
+                            </ul>
                         </li>
-                        <li class="nav-item">
-                            <button className="btn btn-outline-primary mx-2" onClick={onQuickSort}>Quick Sort</button>
+                        <li className="nav-item">
+                            <button className="btn btn-outline-success mx-2" onClick={handleSort}>
+                                Go
+                            </button>
                         </li>
                     </ul>
                 </div>
